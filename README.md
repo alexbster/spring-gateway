47b16e716262ed158a659862dafe5edeea55b13e
alexbster <alexbster@gmail.com>
2023-03-30 19:27:15 -0600

    Configuración de rutas

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spring-gateway
+
+## Requiere
+
+- docker run -d --name redis-stack-server -p 6379:6379 redis/redis-stack-server:latest
---
+++ src/main/java/com/example/spring/gateway/routes/RoutesConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.spring.gateway.routes;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutesConfiguration {
+
+    @Value("${uri.api.clients}")
+    private String uriApiClients;
+
+    /*
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("clients",
+                        p -> p.path("/clients")
+                                .filters(f -> f
+                                        .addRequestHeader("X-Request-Client", "gateway")
+                                        .addResponseHeader("X-Response-Client", "gateway")
+                                        .rewritePath("/clients", "/v1/api/clients")
+                                )
+                        .uri(uriApiClients))
+                .route("clientSearch",
+                        p -> p.path("/clients/search")
+                                .and()
+                                .method("GET")
+                                .and()
+                                .header("X-Subscription-Id", "Curso")
+                                .filters(f -> f
+                                        .addRequestHeader("X-Request-Client", "gateway")
+                                        .addResponseHeader("X-Response-Client", "gateway")
+                                        .rewritePath("/clients/search(?<segment>.*)", "/v1/api/clients/search${segment}")
+                                )
+                                .uri(uriApiClients))
+                .build();
+    }
+    */
+}
